c6e7dffd38fef09799bdc5a258afb88a12872cc7
Michael Carwile <michaelcarwile@users.noreply.github.com>
2015-06-11 08:07:39 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,38 +1,29 @@
-//* Load backstretch js
+<?php
+// copy below this line
 
-/** Add metabox for backstretch default/fallback background image */
-add_action( 'genesis_theme_settings_metaboxes', 'themename_theme_settings_metaboxes', 10, 1 );
-function themename_theme_settings_metaboxes( $pagehook ) {
-
-  add_meta_box( 'themename-default-background-image', __( 'Background - Default Image', 'themename' ), 'themename_default_background_image_metabox', $pagehook, 'main', 'high' );
-
+// Add WordPress settings metabox for default background image
+add_action( 'genesis_theme_settings_metaboxes', 'mc_theme_settings_metaboxes', 10, 1 );
+function mc_theme_settings_metaboxes( $pagehook ) {
+    add_meta_box( 'mc-default-background-image', __( 'Backstretch Default Background Image' ), 'mc_default_background_image_metabox', $pagehook, 'main', 'high' );
+}
+    // Content for the default/fallback image metabox
+function mc_default_background_image_metabox() {
+	printf( '<label for="%s[%s]" /><br />', GENESIS_SETTINGS_FIELD, 'mc_default_image' );
+	printf( '<input type="text" name="%1$s[%2$s]" id="%1$s[%1$s]" size="75" value="%3$s" />', GENESIS_SETTINGS_FIELD, 'mc_default_image', genesis_get_option( 'mc_default_image' ) );
 }
 
-/** Content for the default/fallback image metabox */
-function themename_default_background_image_metabox() {
-
-	printf( '<label for="%s[%s]" /><br />', GENESIS_SETTINGS_FIELD, 'themename_default_image' );
-	printf( '<input type="text" name="%1$s[%2$s]" id="%1$s[%1$s]" size="75" value="%3$s" />', GENESIS_SETTINGS_FIELD, 'themename_default_image', genesis_get_option( 'themename_default_image' ) );
-
+// Enqueue Backstretch JS and prepare images for loading
+add_action( 'wp_enqueue_scripts', 'mc_enqueue_scripts' );
+function mc_enqueue_scripts() {
+    if ( ! genesis_get_option( 'mc_default_image' ) && ! has_post_thumbnail() )
+        return;
+        wp_enqueue_script( 'mc-backstretch', get_bloginfo( 'stylesheet_directory' ) . '/js/backstretch.js', array( 'jquery' ), '1.0.0', true );
+        wp_enqueue_script( 'mc-backstretch-set', get_bloginfo('stylesheet_directory').'/js/backstretch-set.js' , array( 'jquery', 'mc-backstretch' ), '1.0.0', true );
 }
 
-/** Load Backstretch script and prepare images for loading */
-add_action( 'wp_enqueue_scripts', 'themename_enqueue_scripts' );
-function themename_enqueue_scripts() {
-
-if ( ! genesis_get_option( 'themename_default_image' ) && ! has_post_thumbnail() )
-		return;
-
-	wp_enqueue_script( 'themename-backstretch', get_bloginfo( 'stylesheet_directory' ) . '/js/backstretch.js', array( 'jquery' ), '1.0.0', true );
-	wp_enqueue_script( 'themename-backstretch-set', get_bloginfo('stylesheet_directory').'/js/backstretch-set.js' , array( 'jquery', 'themename-backstretch' ), '1.0.0', true );
-
-}
-
-add_action( 'genesis_after', 'themename_set_background_image' );
-function themename_set_background_image() {
-
-	$image = array( 'src' => has_post_thumbnail() ? genesis_get_image( array( 'format' => 'url' ) ) : genesis_get_option( 'themename_default_image' ) );
-
-	wp_localize_script( 'themename-backstretch-set', 'BackStretchImg', $image );
-
+// Set the Backstretch image
+add_action( 'genesis_after', 'mc_set_background_image' );
+function mc_set_background_image() {
+    $image = array( 'src' => has_post_thumbnail() ? genesis_get_image( array( 'format' => 'url' ) ) : genesis_get_option( 'mc_default_image' ) );
+	wp_localize_script( 'mc-backstretch-set', 'BackStretchImg', $image );
 }
